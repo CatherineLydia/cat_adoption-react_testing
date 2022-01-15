d34a3fcc8856bfe5ef2bb7d27ecb9d1b44244b3d
Catherine Lydia <p.cath.reens@gmail.com>
2022-01-15 16:13:46 -0500

    Testing heart button - favorite

--- src/components/Card/__test__/card.test.js
+++ src/components/Card/__test__/card.test.js
@@ -1,11 +1,13 @@
 import { render, screen } from "@testing-library/react";
+import userEvents from "@testing-library/user-event";
 import Card from "../card";
+
 
 const cardProps = {
     name:"Sydney",
     phone:"111-111-1111",
     email:"laith@gmail.com",
-    image:{ url: "https://unsplash.com/photos/83luqSZQgHg", alt: "cute cat image" },
+    image:{ url: "https://unsplash.com/photos/E9kVmtiqqGE", alt: "cute cat image" },
     favorite:false
 }
 
@@ -44,5 +46,33 @@ describe("Card", () => {
     test("should show image with correct src", () => {
         render(<Card {...cardProps} />);
         expect(screen.getByAltText(/cute cat image/i).src).toBe(cardProps.image.url);
+    })
+
+    test("should show outlined heart", () => {
+        render(<Card {...cardProps} />);
+
+        expect(screen.getByAltText(/outlined heart/i)).toBeInTheDocument();
+    });
+
+    test("should show filled heart", () => {
+        render(<Card {...cardProps} favorite={true}/>);
+        expect(screen.queryByAltText(/outlined heart/i)).not.toBeInTheDocument();
+        expect(screen.getByAltText(/filled heart/i)).toBeInTheDocument();
+    });
+
+    test("should toggle heart status", () => {
+        render(<Card {...cardProps} />);
+
+        userEvents.click(screen.getByRole("button"));
+
+        expect(screen.queryByAltText(/outlined heart/i)).not.toBeInTheDocument();
+        expect(screen.getByAltText(/filled heart/i)).toBeInTheDocument();
+
+        userEvents.click(screen.getByRole("button"));
+
+        expect(screen.queryByAltText(/filled heart/i)).not.toBeInTheDocument();
+        expect(screen.getByAltText(/outlined heart/i)).toBeInTheDocument();
+        
+
     })
 });
--- src/components/Card/card.js
+++ src/components/Card/card.js
@@ -1,7 +1,22 @@
-const Card = ({name,phone,email,image,favorite}) => {
+import { useState } from "react";
+import heartFilled from "../../svgs/heartFilled.svg";
+import heartOutlined from "../../svgs/heartOutlined.svg";
+import "./card.css";
+
+const Card = ({ name, phone, email, image, favorite }) => {
+
+    const [isFavorite, setIsFavorite] = useState(favorite);
+    
+    const toggleFavorite = () => {
+        setIsFavorite(!isFavorite);
+    }
+
     return <div className="card">
         <div className="card-header">
             <img src={image.url} alt={image.alt} className="card-img"/>
+            <button className="heart" onClick={toggleFavorite}>
+                {isFavorite? <img src={heartFilled} alt="filled heart"/> :<img src={heartOutlined} alt="outlined heart"/> }
+            </button>
         </div>
         <div className="card-content">
             <h3>{name}</h3>
