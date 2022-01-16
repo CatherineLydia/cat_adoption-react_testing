688988a9430f8b074569c6a603857a0b09792fd5
Catherine Lydia <p.cath.reens@gmail.com>
2022-01-15 21:34:06 -0500

    Client and server

---
+++ client/src/components/Filter/filter.js
@@ -0,0 +1,20 @@
+const Filter = () => <div className="pet-filter-container">
+    <div className="filter-container">
+        <label htmlFor="favourite">Favourite</label>
+        <select name="favourite" id="favourite" className="form-selct">
+            <option value="any">Any</option>
+            <option value="favourite">Favourite</option>
+            <option value="not favourite">Not Favourite</option>
+        </select>
+    </div>
+    <div className="filter-container">
+        <label htmlFor="gender">Gender</label>
+        <select name="gender" id="gender" className="form-selct">
+            <option value="any">Any</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+        </select>
+    </div>
+</div>;
+
+export default Filter;
